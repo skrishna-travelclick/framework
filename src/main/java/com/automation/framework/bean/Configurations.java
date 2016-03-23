431dfcacc24c6c09461de7e5f0565f73c3be8cf0
skrishna-travelclick <skrishna@travelclick.com>
2016-03-23 11:40:58 -0500

    CukesTest

--- src/main/java/com/automation/framework/bean/Configurations.java
+++ src/main/java/com/automation/framework/bean/Configurations.java
@@ -3,6 +3,8 @@ package com.automation.framework.bean;
 public class Configurations {
     private String hostURL; 
     private String seleniumHubUrl;
+    private static String tags;
+    public static final String tag  = "@login";
  //   private int threadCount;
 
     // private Map<String, GridNode> gridNodes;
@@ -24,6 +26,15 @@ public class Configurations {
     public void setSeleniumHubUrl(final String seleniumHubUrl) {
         this.seleniumHubUrl = seleniumHubUrl;
     }
+    
+    public static String getTags() {
+    	return tags;
+    }
+    
+    public void setTags(final String tags) {
+    	this.tags = "@login";
+    }
+    
 
     
     
--- src/test/java/com/automation/framework/runners/CukesTest.java
+++ src/test/java/com/automation/framework/runners/CukesTest.java
@@ -2,11 +2,13 @@ package com.automation.framework.runners;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+import com.automation.framework.bean.*;
+
 
 @CucumberOptions(
         features = {"src/test/resources/SalesForceApp"},//path to the features   		
         plugin = {"pretty", "json:target/cucumber.json"},//what formatters to use
-        tags = {"@login"}
+        tags = {Configurations.tag}
         )//what tags to incluse(@)/exclude(@~)
 
 public class CukesTest extends AbstractTestNGCucumberTests{
